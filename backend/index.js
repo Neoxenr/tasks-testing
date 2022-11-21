7be8eba95a9603db59e617f5aa9a0312ecea5028
Ivan Dolgushin <iv.dolgushin@gmail.com>
2022-11-21 22:02:31 +0600

    deleted unneeded files

--- backend/index.js
+++
@@ -1,1 +0,0 @@
-function isPalindrome(str) { for (let i = 0; i < str.length / 2; i++) { if (str.charAt(i) != str.charAt(str.length - i - 1)) { return false; } } return true; } module.exports = isPalindrome;
